7cb708804f10948f5cbd3f16a94c576b152b21e1
Stuart Robinson <robinson.stu@gmail.com>
2018-11-24 16:40:23 +0000

    Parse wiki search results and load wiki page

--- packages/uw-api/src/components/wiki/wiki-page-controller.ts
+++ packages/uw-api/src/components/wiki/wiki-page-controller.ts
@@ -1,22 +1,24 @@
-// tslint:disable:quotemark
+// tslint:disable:quotemark no-any
 import {Request, Response} from "express"
 import axios from "axios"
 import {ResourceNotFoundException} from "@uw/domain"
 import {fromCharCode} from "@uw/utils"
 import {WikiPage} from "./wiki-model"
+import {search} from "./wiki-search-controller"
 
 const REDIRECT_TEST =
   '<div class=\\"redirectMsg\\"><p>Redirect to:</p><ul class=\\"redirectText\\"><li><a href=\\"/wiki/([0-9a-zA-Z_\\-()%/]*)'
 const RELATIVE_URL_TEST = /(href|src)=(\\?)"(\/(w|wiki)\/)/gi
 const RELATIVE_URL_REPLACE = 'target="_blank" $1="https://en.wikipedia.org$3'
+const INLINE_STYLES_TEST = /(<style.+<\/style>)/gi
 
 export const loadPage = async (req: Request, res: Response) => {
   try {
-    const {category, key, page, redirect} = req.query
+    const {page, redirect} = req.query
     const token = (redirect && redirect) || encodeURIComponent(fromCharCode(page))
     const url = `https://en.wikipedia.org/w/api.php?format=json&action=parse&disabletoc=true&page=${token}`
 
-    console.log("<URL>::", url, res.statusCode, page, token)
+    console.log("<URL::Page>", url, res.statusCode, page, token)
 
     const response = await axios.get(url)
 
@@ -28,47 +30,68 @@ export const loadPage = async (req: Request, res: Response) => {
     const data = await response.data
 
     if (data.error) {
-      const {error} = data
-      const wiki = new WikiPage({
-        category,
-        cp: page,
-        externalLinks: [],
-        key,
-        langlinks: [],
-        text: error.info,
-        title: "Error: " + error.code,
-        type: "error",
-      })
-      res.status(res.statusCode).json(wiki)
+      onError(req, res, data)
     } else {
-      const {parse} = data
-      let text = parse.text["*"]
-
-      const redirect = text.match(REDIRECT_TEST)
-
-      if (redirect) {
-        // wiki api return a redirect document
-        // -> follow the link
-        req.query.redirect = redirect[1]
-        await loadPage(req, res)
-      } else {
-        text = text.replace(RELATIVE_URL_TEST, RELATIVE_URL_REPLACE)
-
-        const wiki = new WikiPage({
-          category,
-          cp: page,
-          externalLinks: parse.externallinks,
-          key,
-          langlinks: parse.langlinks,
-          text,
-          title: parse.displaytitle,
-          type: "page",
-        })
-        res.status(res.statusCode).json(wiki)
-      }
+      onSuccess(req, res, data)
     }
   } catch (e) {
     console.log(e)
+  }
+}
+
+const onError = async (req: Request, res: Response, data: any) => {
+  const {category, key, page} = req.query
+  const {error} = data
+  if (error.code === "missingtitle") {
+    // page not found -> get page from search results
+    req.query.return = true
+    await search(req, res)
+    const urls = res.locals.wikiSearch.results[3]
+    const uUrl = urls.filter((url: string) => url.match(/unicode/i))
+    const url = (uUrl.length && uUrl[0]) || urls[0]
+    const parts = url.split("/")
+    const page = parts[parts.length - 1]
+    req.query.redirect = page
+    await loadPage(req, res)
+  } else {
+    const wiki = new WikiPage({
+      category,
+      cp: page,
+      externalLinks: [],
+      key,
+      langlinks: [],
+      text: error.info,
+      title: "Error: " + error.code,
+      type: "error",
+    })
+    res.status(res.statusCode).json(wiki)
+  }
+}
+
+const onSuccess = async (req: Request, res: Response, data: any) => {
+  const {category, key, page} = req.query
+  const {parse} = data
+  let text = parse.text["*"]
+  const redirect = text.match(REDIRECT_TEST)
+
+  if (redirect) {
+    // if wiki api returns a redirect document
+    // -> follow the link
+    req.query.redirect = redirect[1]
+    await loadPage(req, res)
+  } else {
+    text = text.replace(RELATIVE_URL_TEST, RELATIVE_URL_REPLACE).replace(INLINE_STYLES_TEST, "")
+    const wiki = new WikiPage({
+      category,
+      cp: page,
+      externalLinks: parse.externallinks,
+      key,
+      langlinks: parse.langlinks,
+      text,
+      title: parse.displaytitle,
+      type: "page",
+    })
+    res.status(res.statusCode).json(wiki)
   }
 }
 
