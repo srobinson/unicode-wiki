899e9c55c4ea2caea081fcd86e7bd182a6511b0f
Stuart Robinson <robinson.stu@gmail.com>
2018-11-27 20:56:27 +0000

    ..

--- packages/uw-api/src/db/mongo/connect.ts
+++ packages/uw-api/src/db/mongo/connect.ts
@@ -3,6 +3,7 @@ import {jsonifyError, InternalException} from "@uw/domain"
 import {logger} from "@uw/logging"
 import {sleep} from "@uw/utils"
 import "../../config"
+import {jsonifyError} from "@uw/domain"
 
 export const DB_URL = `${process.env.MONGO_URL}/unicode-wiki`
 
@@ -11,38 +12,21 @@ export class MongoDb {
   public static reconnectTries: number = 60
   public static retries: number = 0
 
-  // tslint:disable:no-any
-  public static connect = async (): Promise<any> => {
-    return mongoose
-      .connect(
-        DB_URL,
-        {
-          useNewUrlParser: true,
-        },
-      )
-      .then(db => {
-        logger.info(`===>  Connected! to ${DB_URL}`)
-        const connection = db.connection
-        connection.on("error", (e: Error) => logger.error(jsonifyError(e)))
-        return db
-      })
-      .catch((e: Error) => {
-        if (MongoDb.retries < MongoDb.reconnectTries) {
-          logger.error(
-            Object.assign(jsonifyError(e), {
-              retry: `Retry attempt [${++MongoDb.retries}/${
-                MongoDb.reconnectTries
-              }] in ${MongoDb.reconnectInterval / 1000}s`,
-            }),
-          )
-          sleep(MongoDb.reconnectInterval)
-          logger.info(`Connecting to ${DB_URL}`)
-          return MongoDb.connect()
-        } else {
-          throw new InternalException(
-            new Error(`Could not connect to Mongo after ${MongoDb.retries} retries`),
-          )
-        }
-      })
+  public static connect = async () => {
+    if (!MongoDb.connection) {
+      try {
+        await mongoose.connect(
+          DB_URL,
+          {useNewUrlParser: true},
+        )
+        const connection = mongoose.connection
+        connection.on("open", () => logger.info(`===>  Connected to ${DB_URL}`))
+        connection.on("error", (e: Error) => logger.error(jsonifyError))
+        MongoDb.connection = connection
+      } catch (e) {
+        logger.error(jsonifyError(e))
+        process.exit()
+      }
+    }
   }
 }
