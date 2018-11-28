ae07f4d1ba46c3e1ac66aa41ec3116138359aac5
Stuart Robinson <robinson.stu@gmail.com>
2018-11-28 00:07:53 +0000

    code review

--- packages/uw-logging/src/logger.ts
+++ packages/uw-logging/src/logger.ts
@@ -3,23 +3,11 @@ import {createLogger, format, transports} from "winston"
 
 const MESSAGE = Symbol.for("message")
 
-// tslint:disable:object-literal-sort-keys no-any
-
 const jsonFormatter = (logEntry: any) => {
   const base = {timestamp: new Date(), ...logEntry}
   logEntry[MESSAGE] = JSON.stringify(base, undefined, 2)
   return logEntry
 }
-
-// const logFormat = json((info: any) => {
-//   console.log("XXX", info)
-
-//   return {
-//     level: info.level,
-//     date: info.timestamp,
-//     message: info.message,
-//   }
-// })
 
 const logger = createLogger({
   exceptionHandlers: [
@@ -28,7 +16,6 @@ const logger = createLogger({
     }),
   ],
   exitOnError: false,
-  // format: combine(timestamp(), logFormat),
   format: format(jsonFormatter)(),
   transports: [
     new transports.Console({
@@ -37,16 +24,6 @@ const logger = createLogger({
     }),
   ],
 })
-
-// export const requestInfoMessage = (req: Request) => ({
-//   request: {
-//     method: req.method,
-//     requestId: req.requestId,
-//     url: req.originalUrl,
-//     user: req.user || "guest",
-//     userAgent: req.headers["user-agent"],
-//   },
-// })
 
 logger.on("error", err => {
   console.log("OOOPS! Something bad happened with winston", err)
