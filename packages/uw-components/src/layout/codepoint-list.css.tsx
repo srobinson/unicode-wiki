b7e5375dc7b644c63ac2ef669733486cb299e326
Stuart Robinson <robinson.stu@gmail.com>
2018-12-02 08:55:27 +0000

    PR:mods

--- packages/uw-components/src/layout/codepoint-list.css.tsx
+++ packages/uw-components/src/layout/codepoint-list.css.tsx
@@ -2,7 +2,6 @@ import styled from "../styled"
 
 export const CodepointList = styled("div")`
   display: flex;
-  /* justify-content: space-between; */
   align-content: stretch;
   flex-wrap: wrap;
 `
