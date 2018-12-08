20a5d317fda73579c98125b312abce11c12506f0
Stuart Robinson <robinson.stu@gmail.com>
2018-12-08 08:53:13 +0000

    update deploy config

--- deploy.sh
+++ deploy.sh
@@ -15,18 +15,22 @@ read_var() {
     echo ${VAR[1]}
 }
 
-function deploy() {
+deploy() {
   if ! docker images | grep -v 'grep' | grep -q "$1.*$2"; then
-    echo "building gcr.io/unicode-wiki/uw-$1:$2 ..."
-    docker build -f Dockerfile.$1 -t gcr.io/unicode-wiki/uw-$1:$2 . || exit 3
-    echo "docker build gcr.io/unicode-wiki/uw-$1:$2 complete"
+    echo building gcr.io/unicode-wiki/uw-$1:$2 $3 $4 ...
+    if [ $1 == "api" ]; then
+      docker build --no-cache --build-arg MONGO_URL=$3 --build-arg ES_URL=$4 -f Dockerfile.$1 -t gcr.io/unicode-wiki/uw-$1:$2 . || exit 3
+    elif [ $1 == "app" ]; then
+      docker build --no-cache --build-arg API_URL=$3 --build-arg FONTS_URL=$4 -f Dockerfile.$1 -t gcr.io/unicode-wiki/uw-$1:$2 . || exit 3
+    fi
+    echo docker build gcr.io/unicode-wiki/uw-$1:$2 complete
   fi
   if ! gcloud container images list-tags gcr.io/unicode-wiki/uw-$1 | grep -v 'grep' | grep -q "$1.*$2"; then
-    echo "pushing gcr.io/unicode-wiki/uw-$1:$2"
+    echo pushing gcr.io/unicode-wiki/uw-$1:$2
     docker push gcr.io/unicode-wiki/uw-$1:$2 || exit 3
-    echo "gcr.io/unicode-wiki/uw-$1:$2 pushed"
+    echo gcr.io/unicode-wiki/uw-$1:$2 pushed
     kubectl set image deployment/uw-$1-web uw-$1-web=gcr.io/unicode-wiki/uw-$1:$2 || exit 3
-    echo "gcr.io/unicode-wiki/uw-$1:$2 deployed"
+    echo gcr.io/unicode-wiki/uw-$1:$2 deployed
   fi
 }
 
@@ -35,6 +39,6 @@ then
   >&2 echo error
 fi
 
-deploy api $(read_var VERSION_API .env)
-deploy app $(read_var VERSION_APP .env)
+deploy api $(read_var VERSION_API .env-prod) $(read_var MONGO_URL .env-prod) $(read_var ES_URL .env-prod)
+# deploy app $(read_var VERSION_APP .env-prod) $(read_var API_URL .env-prod) $(read_var FONTS_URL .env-prod)
 
