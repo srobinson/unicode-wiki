31127b68cf52fa93a4d8665b8a3c184794ab737e
Stupot <robinson.stu@gmail.com>
2019-01-05 02:56:07 +0000

    ci(travis): version/deploy

--- .version.sh
+++ .version.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
+echo TRAVIS_BRANCH: $TRAVIS_BRANCH
+
 if [[ $TRAVIS_BRANCH == 'master' ]]; then
+
+  # get current version
   v=$(git describe --tags `git rev-list --tags --max-count=1`)
-  npx oao publish --no-confirm --new-version ${v#"v"}
-  exec ./deploy.sh
+  # get last commit
+  m=`git show --pretty`
+
+  if grep -q 'BREAKING\sCHANGE:' <<< $m; then
+    nv=`./.increment_version.sh -M ${v#"v"}`
+  elif grep -q feat\([a-z]*\): <<< $m; then
+    nv=`./.increment_version.sh -m ${v#"v"}`
+  else
+    nv=`./.increment_version.sh -p ${v#"v"}`
+  fi
+
+  npx oao publish --no-confirm --new-version v$nv
+  ./deploy.sh
+
 fi
+
+# 0.0.1
+# feat: A new feature 0.1.0
+# fix: A bug fix 0.1.1
+# docs: Documentation only changes 0.1.2
+# style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc) 0.1.3
+# refactor: A code change that neither fixes a bug nor adds a feature
+# perf: A code change that improves performance
+# test: Adding missing or correcting existing tests
+# chore: Changes to the build process or auxiliary tools and libraries such as documentation generation
+
