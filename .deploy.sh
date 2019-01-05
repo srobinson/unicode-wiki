4b365a3c02bd69995aa57a092189e0c3dcc37e3f
Stuart Robinson <robinson.stu@gmail.com>
2019-01-05 21:54:15 +0000

    ci(gcloud): build/deploy

--- .deploy.sh
+++ .deploy.sh
@@ -14,11 +14,9 @@ packages_arr=($(echo $packages | tr " " "\n"))
 
 auth() {
   if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; curl https://sdk.cloud.google.com | bash > /dev/null; fi
-  # Add gcloud to $PATH
   source /home/travis/google-cloud-sdk/path.bash.inc
   gcloud version
   gcloud --quiet components update kubectl
-  # Auth flow
   echo $GCLOUD_KEY | base64 --decode > gcloud.p12
   gcloud auth activate-service-account $GCLOUD_EMAIL --key-file gcloud.p12
   ssh-keygen -f ~/.ssh/google_compute_engine -N ""
