38e82e1953ca6d3ef788d698811b96ad0dd23807
Stuart Robinson <robinson.stu@gmail.com>
2019-01-05 22:20:18 +0000

    ci(gcloud): build/deploy

--- .deploy.sh
+++ .deploy.sh
@@ -19,6 +19,7 @@ auth() {
   gcloud --quiet components update kubectl
   echo $GCLOUD_KEY | base64 --decode > gcloud.p12
   gcloud auth activate-service-account $GCLOUD_EMAIL --key-file gcloud.p12
+  gcloud auth configure-docker
   ssh-keygen -f ~/.ssh/google_compute_engine -N ""
 }
 
