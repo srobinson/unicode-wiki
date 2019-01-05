155f6bc98eecbadd77a7ca78784da5b2e1dc68ce
Stupot <robinson.stu@gmail.com>
2019-01-05 00:12:32 +0000

    ci(seed): ensure db:seed only runs once
    #43

--- .version.sh
+++ .version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'master' ]]
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
   v=$(git describe --tags `git rev-list --tags --max-count=1`)
-  npx oao publish --no-confirm --new-version $v
+  npx oao publish --no-confirm --new-version ${v#"v"}
   exec ./deploy.sh
 fi
