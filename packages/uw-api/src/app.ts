fc9f33bef1156a44fb16ff158ae94343bbd0122d
Stuart Robinson <robinson.stu@gmail.com>
2019-01-07 21:03:32 +0000

    chore(travis): test build breaks
    affects: @uw/api

--- packages/uw-api/src/app.ts
+++ packages/uw-api/src/app.ts
@@ -9,6 +9,7 @@ import Routes from "./routes"
 import "express-async-errors"
 import "./config"
 
+// test broken build
 class Express {
   public static config(): express.Application {
     const app: express.Application = express()
