6baf52c68fa7dac50b0bef3e3a19cfc6784c84bd
Stuart Robinson <robinson.stu@gmail.com>
2019-01-07 12:07:58 +0000

    feat(test): test new feat deployment

--- packages/uw-api-graph/src/server.ts
+++ packages/uw-api-graph/src/server.ts
@@ -3,7 +3,7 @@ import {schema} from "./schema"
 import {dataSources} from "./datasources"
 import "./config"
 
-// NEW FEAT
+// NEW FEAT TEST!
 const server = new ApolloServer({
   dataSources,
   introspection: true,
