9f5db10b1d51a6b1b27dfbf29824d14e8c2335cc
Stuart Robinson <robinson.stu@gmail.com>
2019-01-07 13:31:23 +0000

    v0.3.0

--- packages/uw-api-graph/src/server.ts
+++ packages/uw-api-graph/src/server.ts
@@ -3,7 +3,7 @@ import {schema} from "./schema"
 import {dataSources} from "./datasources"
 import "./config"
 
-// NEW FEAT TEST!!
+// NEW FEAT TEST!! 0.3
 const server = new ApolloServer({
   dataSources,
   introspection: true,
